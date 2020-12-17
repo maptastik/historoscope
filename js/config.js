4d2272f5cef58be47c614dc18c7f64f83c843f65
Ryan Cooper <ryancooper729@gmail.com>
2020-12-17 13:30:03 -0500

    Adapt for Raleigh imagery services

--- js/config.js
+++ js/config.js
@@ -1,43 +1,48 @@
 var configFile = {
   // url to services
-  base_URL: "https://maps.hayward-ca.gov/arcgis/rest/services/Ext_Basemaps",
+  base_URL: "https://maps.raleighnc.gov/images/rest/services",
 
   // initial map on left hand map frame
-  defaultLeft: "AerialPhotoHybrid_2017",
+  defaultLeft: "Orthos2019",
 
   // initial map on right hand map frame
-  defaultRight: "AerialPhoto_1968_12inch",
+  defaultRight: "Orthos1981",
 
   // rename map services names to user friendly names (if needed)
   rename: [
-    ["Aerial_Imagery_1954", "1954 Aerial"],
-    ["AerialPhoto_1968_12inch", "1968 Aerial"],
-    ["AerialPhoto_1985_21inch", "1985 Aerial"],
-    ["AerialPhoto_1992_12inch", "1992 Aerial"],
-    ["AerialPhoto_1999_6inch", "1999 Aerial"],
-    ["AerialPhoto_2002_24inch", "2002 Aerial"],
-    ["AerialPhoto_2003_12inch", "2003 Aerial"],
-    ["AerialPhoto_2004_6inch", "2004 Aerial"],
-    ["AerialPhoto_2006_4inch", "2006 Aerial"],
-    ["AerialPhoto_2009_6inch", "2009 Aerial"],
-    ["AerialPhoto_2011_12inch", "2012 Aerial"],
-    ["AerialPhoto_2014_3inch", "2014 Aerial"],
-    ["AerialPhotoHybrid_2014", "2014 Aerial (w/labels)"],
-    ["AerialPhotoHybrid_2017", "2017 Aerial (w/labels)"],
-    ["bm_COHParcelPublicAccess", "Hayward Basemap"],
-    ["UAV_Library_20190318", "New Library - March 2019"]
+    ["Orthos1981", "1981 Aerial"],
+    ["Orthos1988", "1988 Aerial"],
+    ["Orthos1999", "1999 Aerial"],
+    ["Orthos2002", "2002 Aerial"],
+    ["Orthos2004", "2004 Aerial"],
+    ["Orthos2005", "2005 Aerial"],
+    ["Orthos2006", "2006 Aerial"],
+    ["Orthos2007", "2007 Aerial"],
+    ["Orthos2008", "2008 Aerial"],
+    ["Orthos2009", "2009 Aerial"],
+    ["Orthos2010", "2010 Aerial"],
+    ["Orthos2011", "2011 Aerial"],
+    ["Orthos2012", "2012 Aerial"],
+    ["Orthos2013", "2013 Aerial"],
+    ["Orthos2014", "2014 Aerial"],
+    ["Orthos2015", "2015 Aerial"],
+    ["Orthos2016", "2016 Aerial"],
+    ["Orthos2017", "2017 Aerial"],
+    ["Orthos2018", "2018 Aerial"],
+    ["Orthos2019", "2019 Aerial"],
   ],
 
   // services to exclude from Historoscope
   exclude: [
-    "Elevation2014",
-    "Slope2014",
-    "UAV_HARDShoreline_KingTide_20180102",
-    "UAV_HARDShoreline_LowTide_20180305",
-    "COH_Dark_Gray"
+    // "Elevation2014",
+    // "Slope2014",
+    // "UAV_HARDShoreline_KingTide_20180102",
+    // "UAV_HARDShoreline_LowTide_20180305",
+    // "COH_Dark_Gray"
   ],
 
   mapCoords: [
-    37.67, -122.09
-  ]
+    35.77717,-78.646118
+  ],
+  mapZoom: 17
 }
